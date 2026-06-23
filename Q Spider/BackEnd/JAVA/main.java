a8d0950f87592acf47d96168d291706b0ec3c92b
kunalfukat2-sketch <kunalfukat2@gmail.com>
2026-06-23 12:31:00 +0530

    Web-Tech
    Learn About the Html ,Css, java.

---
+++ Q Spider/BackEnd/JAVA/main.java
@@ -0,0 +1,17 @@
+class Demo
+
+{
+   public static void main(String[] args) {
+
+      System.out.println("Kunal");
+      System.out.println("gender:" + "male");
+      System.out.println("age" + 22);
+      System.out.println(7821942320l);
+      System.out.println(5.8f);
+      System.out.println("kunalfukat111@gmail.com");
+      System.out.println(72);
+      System.out.println("Body count :" + 0);
+
+   }
+
+}
