142ecf99c53832dbc62678102a0e6f3398fe1b20
kunalfukat2-sketch <kunalfukat2@gmail.com>
2026-06-25 12:55:32 +0530

    css
    box model and backgroud image proparties

---
+++ Q Spider/BackEnd/JAVA/Arithmatic operaters/Arithamatic.java
@@ -0,0 +1,214 @@
+class Arithamatic {
+
+    public static void main(String[] args) {
+
+        // //!Addiction
+        // byte b1 = 100;
+        // byte b2 = 50;
+        // int result = b1 + b2;
+        // System.out.println(result);
+
+        // short a = 200;
+        // short b = 500;
+        // int result = a + b;
+        // System.out.println(result);
+
+        // char a = 'a';
+        // char b = 'b';
+        // int res ult = a + b;
+        // System.out.println(result);
+
+        // in t =50000;
+        // in t =20000;
+        // int res u l = a + b;
+        // System.out.println(result);
+
+        // float a = 2.1f;
+        // float b = 1.1f;
+        // float result = a + b;
+        // System.out.println(result);
+
+        // double a = 1.1;
+        // double b = 2.0;
+        // double result = a + b;
+        // System.out.println(result);
+
+        // boolean a = true;
+        // boolean b = false;
+        // boolean result = a + b;
+        // System.out.println(result);
+
+        // String a = "kunal";
+        // String b = "Fukat";
+        // String result = a + b;
+        // System.out.println(result);
+
+        // //! Substraction
+        // byte b1 = 100;
+        // byte b2 = 50;
+        // int result = b1 - b2;
+        // System.out.println(result);
+
+        // short a = 200;
+        // short b = 500;
+        // int result = a - b;
+        // System.out.println(result);
+
+        // char a = 'a';
+        // char b = 'b';
+        // int res ult = a-b;
+        // System.out.println(result);
+
+        // in t =50000;
+        // in t =20000;
+        // int res u l =a-b;
+        // System.out.println(result);
+
+        // float a = 2.1f;
+        // float b = 1.1f;
+        // float result = a - b;
+        // System.out.println(result);
+
+        // double a = 1.1;
+        // double b = 2.0;
+        // double result = a - b;
+        // System.out.println(result);
+
+        // boolean a = true;
+        // boolean b = false;
+        // boolean result = a - b;
+        // System.out.println(result);
+
+        // String a = "kunal";
+        // String b = "Fukat";
+        // String result = a - b;
+        // System.out.println(result);
+
+        // //! Multiplaction
+
+        // byte b1 = 100;
+        // byte b2 = 50;
+        // int result = b1 * b2;
+        // System.out.println(result);
+
+        // short a = 200;
+        // short b = 500;
+        // int result = a * b;
+        // System.out.println(result);
+
+        // char a = 'a';
+        // char b = 'b';
+        // int res ult = a *b;
+        // System.out.println(result);
+
+        // in t a=50000;
+        // in t b=20000;
+        // int res u l t =a*b;
+        // System.out.println(result);
+
+        // float a = 2.1f;
+        // float b = 1.1f;
+        // float result = a * b;
+        // System.out.println(resul
+
+        // double a = 1.1;
+        // double b = 2.0;
+        // double result = a * b;
+        // System.out.println(result);
+
+        // boolean a = true;
+        // boolean b = false;
+        // boolean result = a * b;
+        // System.out.println(result);
+
+        // String a = "kunal";
+        // String b = "Fukat";
+        // String result = a * b;
+        // System.out.println(result);
+
+        // //!Division------>
+
+        // byte b1 = 100;
+        // byte b2 = 50;
+        // int result = b1 / b2;
+        // System.out.println(result);
+
+        // short a = 200;
+        // short b = 500;
+        // int result = a / b;
+        // System.out.println(result);
+
+        // char a = 'a';
+        // char b = 'b';
+        // int res ult = a / b;
+        // System.out.println(result);
+
+        // in t a=50000;
+        // in t b=20000;
+        // int res u l t =a / b;
+        // System.out.println(result);
+
+        // float a = 2.1f;
+        // float b = 1.1f;
+        // float result = a / b;
+        // System.out.println(resul
+
+        // double a = 1.1;
+        // double b = 2.0;
+        // double result = a / b;
+        // System.out.println(result);
+
+        // boolean a = true;
+        // boolean b = false;
+        // boolean result = a / b;
+        // System.out.println(result);
+
+        // String a = "kunal";
+        // String b = "Fukat";
+        // String result = a / b;
+        // System.out.println(result);
+
+        // //! Modules
+
+        // byte b1 = 100;
+        // byte b2 = 50;
+        // int result = b1 % b2;
+        // System.out.println(result);
+
+        // short a = 200;
+        // short b = 500;
+        // int result = a % b;
+        // System.out.println(result);
+
+        // char a = 'a';
+        // char b = 'b';
+        // int res ult = a % b;
+        // System.out.println(result);
+
+        // in t a=50000;
+        // in t b=20000;
+        // int res u l t =a % b;
+        // System.out.println(result);
+
+        // float a = 2.1f;
+        // float b = 1.1f;
+        // float result = a % b;
+
+        // double a = 1.1;
+        // double b = 2.0;
+        // double result = a % b;
+        // System.out.println(result);
+
+        // boolean a = true;
+        // boolean b = false;
+        // boolean result = a % b;
+        // System.out.println(result);
+
+        // String a = "kunal";
+        // String b = "Fukat";
+        // String result = a % b;
+        // System.out.println(result);
+
+    }
+
+}
